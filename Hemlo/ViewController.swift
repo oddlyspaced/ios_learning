d1820eafa9a1b955df1796539453fee4d0ddf993
oddlyspaced <oddlyspaced1@gmail.com>
2020-10-28 20:51:29 +0530

    Setup basic ticker app

--- Hemlo/ViewController.swift
+++ Hemlo/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var counter = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func click(_ sender: Any) {
+        label.text = String(counter)
+        counter = counter + 1
+    }
+    
 
 }
 
